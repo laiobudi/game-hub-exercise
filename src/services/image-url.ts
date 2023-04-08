cf318bf14a36b09a3935be87efe16a8645cee6db
Frank Leo <lrf261812@gmail.com>
2023-04-08 22:00:52 +1000

    Sorted the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,8 @@
+// TODO: Some games have no image url. They are currently bypassed with a if statement.
+
 const getCroppedImageUrl = (url: string) => {
+	if (!url) return "";
+
 	const target = "media/";
 	const index = url.indexOf(target) + target.length;
 	return url.slice(0, index) + "crop/600/400/" + url.slice(index);
