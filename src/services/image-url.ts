d0eadf8a148ac48787a4caa73aed657442e155ec
Frank Leo <lrf261812@gmail.com>
2023-04-08 22:13:18 +1000

    Handle games without an image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,7 @@
-// TODO: Some games have no image url. They are currently bypassed with a if statement.
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 
 const getCroppedImageUrl = (url: string) => {
-	if (!url) return "";
+	if (!url) return noImage;
 
 	const target = "media/";
 	const index = url.indexOf(target) + target.length;
