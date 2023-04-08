7ce04b6549a1a8d48bed161e423b777e83a0f4d7
Frank Leo <lrf261812@gmail.com>
2023-04-08 15:48:11 +1000

    Highlight selected Genre.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
 	onSelectGenre: (genre: Genre) => void;
+	selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 	const { data, isLoading, error } = useGenres();
 
 	if (error) return null;
@@ -32,6 +33,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
 						<Button
 							variant="link"
 							onClick={() => onSelectGenre(genre)}
+							fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
 							fontSize="lg"
 						>
 							{genre.name}
