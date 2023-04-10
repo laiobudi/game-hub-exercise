816fc0a657008f27bf88f9fac09e14e85738ba62
Frank Leo <lrf261812@gmail.com>
2023-04-10 14:59:16 +1000

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,4 @@
+import genre from "../data/genre";
 import useData from "./useData";
 
 ////////////////////
@@ -11,7 +12,11 @@ export interface Genre {
 	name: string;
 	image_background: string;
 }
+// const useGenres = () => useData<Genre>("/genres");
+// This is commented so that the app does not query game data every time it loads.
+// In the case that data on the backend doesn't change as often, the app will load quicker.
+// To change this back, comment the following line and uncomment the top line.
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genre, isLoading: false, error: null });
 
 export default useGenres;
